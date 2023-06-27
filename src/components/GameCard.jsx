a9fb7d0008cde8b6bf45224f469af91bf6042223
shehroz khan <sherozkhan321@gmail.com>
2023-06-27 01:16:10 -1000

    displaying platform icons

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,18 @@
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+
+const GameCard = ({ game }) => {
+  return (
+    <Card borderRadius={10} overflow={"hidden"}>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
---
+++ src/components/PlatformIconList.jsx
@@ -0,0 +1,41 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+
+import { HStack, Icon } from "@chakra-ui/react";
+
+const PlatformIconList = ({ platforms }) => {
+  const iconMap = {
+    pc: FaWindows,
+    playStation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color={"gray.500"}
+        />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;

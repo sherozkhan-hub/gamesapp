7a0cf0b54745e5b41a4397bd4fca79de6c58e1da
shehroz khan <sherozkhan321@gmail.com>
2023-06-28 01:41:58 -1000

    show skeletons

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -5,7 +5,7 @@ import CriticScore from "./CriticScore";
 
 const GameCard = ({ game }) => {
   return (
-    <Card borderRadius={10} overflow={"hidden"}>
+    <Card borderRadius={10} overflow={"hidden"} width={"300px"}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
--- src/hooks/useGames.js
+++ src/hooks/useGames.js
@@ -5,23 +5,28 @@ import { CanceledError } from "axios";
 const useGames = () => {
   const [games, setGames] = useState([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const controller = new AbortController();
+
     apiClient
       .get("/games", { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results);
+        setLoading(false);
       })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
-  });
+  }, []);
 
-  return { error, games };
+  return { error, games, isLoading };
 };
 
 export default useGames;
